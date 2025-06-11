6a421e2fbbdefdabf34398cce87d47a433e0e46e
NorthXyRan <2405358569@qq.com>
2025-06-11 15:32:25 +0800

    Update main.ts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,14 +1,17 @@
 import './assets/main.css'
 import router from './router'
 
-import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import { createPinia } from 'pinia'
+import { createApp } from 'vue'
 import App from './App.vue'
 
 const app = createApp(App)
+const pinia = createPinia()
 
-app.use(ElementPlus, { size: 'small', zIndex: 3000 })            // 使用ElementPlus，设置表单组件的默认尺寸为small，zIndex为3000
+app.use(pinia)
+app.use(ElementPlus, { size: 'small', zIndex: 3000 }) // 使用ElementPlus，设置表单组件的默认尺寸为small，zIndex为3000
 app.use(router)
 
 app.mount('#app')
